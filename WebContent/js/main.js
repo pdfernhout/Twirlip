ed71d1e008494a838d6cd2c1c88c4005fb8fa002
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-02-02 16:17:19 -0500

    Getting a Dojo Textarea up on page in a ContentPane

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,1 +1,18 @@
 console.log("Need to do helpful stuff here");
+
+require(["dijit/layout/ContentPane", "dijit/form/Textarea", "dojo/domReady!"], function(ContentPane, Textarea) {
+    var contentPane = new ContentPane({
+        content:"<p>I am initial content</p>"
+    });
+    
+    document.body.appendChild(contentPane.domNode);
+    contentPane.startup();
+    
+    var textarea = new Textarea({
+        name: "myarea",
+        value: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+        style: "width: 800px;"
+    });
+    
+    textarea.placeAt(contentPane);
+});
