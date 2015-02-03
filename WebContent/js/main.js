4e78a58868146b087e38e6e1f16cf2b1cae41539
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-02-02 20:02:22 -0500

    Adding simple save functionality to main.js.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,19 +1,31 @@
 console.log("Starting Twirlip");
 
-require(["dijit/form/Button", "dijit/layout/ContentPane", "dijit/form/Textarea", "dojo/domReady!"],
-function(Button, ContentPane, Textarea) {
+require(["dijit/form/Button", "dijit/layout/ContentPane", "js/pointrel20141201Client", "dijit/form/Textarea", "dojo/domReady!"],
+function(Button, ContentPane, pointrel20141201Client, Textarea) {
+    var textarea;
+    
     function saveClicked() {
+        var text = textarea.get("value");
         console.log("Save clicked");
+        var metadata = {id: "test", contentType: "text/plain", committer: "test", timestamp: true};        
+        pointrel20141201Client.storeInNewEnvelope(text, metadata, function(error, serverResponse) {
+            if (error) {
+                console.log("could not write new version:\n" + error);
+                return;
+            }
+            var sha256HashAndLength = serverResponse.sha256AndLength;
+            console.log("wrote sha256HashAndLength:", sha256HashAndLength);
+        });
     }
     
     var contentPane = new ContentPane({
-        content:"<p>I am initial content</p>"
+        content:"<p>Test of saving document</p>"
     });
     
     document.body.appendChild(contentPane.domNode);
     contentPane.startup();
     
-    var textarea = new Textarea({
+    textarea = new Textarea({
         name: "myarea",
         value: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
         style: "width: 800px;"
