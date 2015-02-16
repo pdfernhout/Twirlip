a4eca777b5d06d6dfd0f8f768b7cc0499894f8a2
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2015-02-16 10:35:17 -0500

    Update TwirlipServer.js
    Improved how web page content is returned if it is not a string but is JSON object

--- server/TwirlipServer.js
+++ server/TwirlipServer.js
@@ -117,6 +117,12 @@ function getPointrelContent(request, response) {
             content = new Buffer(content, 'base64');
         }
         response.setHeader('content-type', contentType);
+        if (typeof content !== 'string') {
+            // Application is storing content directly as nested JSON, so make it displayable as a string
+            content = JSON.stringify(content, null, 2);
+            // TODO: Changed the content type returned so that it is viewable; is this OK?
+            contentType = "application/json";
+        }
         return response.end(content);
     });
 }
